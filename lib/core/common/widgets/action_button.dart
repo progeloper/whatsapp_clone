f5ca048fbbbd5e14ce2c83b3f732c735d2e6311f
Mayeavx <mayoodukoya@gmail.com>
2023-05-25 21:21:17 +0100

    initial build

---
+++ lib/core/common/widgets/action_button.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ActionButton extends StatelessWidget {
+  final Color color;
+  final String text;
+  const ActionButton({Key? key, required this.text, required this.color}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/features/auth/screens/welcome_screen.dart
+++ lib/features/auth/screens/welcome_screen.dart
@@ -8,12 +8,54 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Text('Welcome to WhatsApp', style: TextStyle(
-              color: Palette.textColor,
-            ),),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0).copyWith(
+            top: 32,
+            bottom: 16,
+          ),
+          child: Container(
+            height: double.infinity,
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Welcome to WhatsApp',
+                  style: TextStyle(
+                    color: Palette.textColor,
+                    fontSize: 20,
+                  ),
+                ),
+                Flexible(
+                  child: Container(),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  child: Image.asset(
+                    'assets/bg.png',
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Flexible(child: Container()),
+                RichText(
+                  textAlign: TextAlign.center,
+                    text: TextSpan(
+                  text: 'Tap "Agree and continue" to accept the ',
+                  style: TextStyle(
+                    color: Palette.textColor,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'WHatsApp Terms of Service and Privacy Policy',
+                      style: TextStyle(
+                        color: Palette.messageColor,
+                      ),
+                    ),
+                  ],
+                ),),
+              ],
+            ),
+          ),
         ),
       ),
     );
